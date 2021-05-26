f5c5e95455b8a1873bc6dde10158446808766b83
Kesinee Powers <56649145+KesineeP@users.noreply.github.com>
2021-05-26 15:00:23 -0400

    Refactor duplicate code and added displayMessage method

--- script.js
+++ script.js
@@ -9,10 +9,13 @@
 // document.querySelector(".guess").value = 23;
 // console.log(document.querySelector(".guess").value);
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 let highscore = 0;
 
+const displayMessage = (message) => {
+  document.querySelector(".message").textContent = message;
+};
 document.querySelector(".number").textContent = "?";
 
 //Check button
@@ -22,49 +25,39 @@ document.querySelector(".check").addEventListener("click", () => {
 
   //When there is no input
   if (!guess) {
-    document.querySelector(".message").textContent = "No Number!";
+    displayMessage("No Number!");
 
     //When player wins
   } else if (guess === secretNumber) {
-    document.querySelector(".message").textContent = "Correct number!";
+    displayMessage("Correct Number!");
+    document.querySelector(".number").textContent = secretNumber;
     document.querySelector("body").style.backgroundColor = "#60b347";
     document.querySelector(".number").style.width = "30rem";
-    document.querySelector(".number").textContent = secretNumber;
 
     if (score > highscore) {
       highscore = score;
       document.querySelector(".highscore").textContent = highscore;
     }
     //When guess is too high
-  } else if (guess > secretNumber) {
+  } else if (guess !== secretNumber) {
     if (score > 1) {
-      document.querySelector(".message").textContent = "Too high!";
+      displayMessage(guess > secretNumber ? "Too high!" : "Too low!");
       score--;
       document.querySelector(".score").textContent = score;
     } else {
-      document.querySelector(".message").textContent = "You lost the game!";
-      document.querySelector(".score").textContent = 0;
-    }
-
-    //When guess is too low
-  } else if (guess < secretNumber) {
-    if (score > 1) {
-      document.querySelector(".message").textContent = "Too low!";
-      score--;
-      document.querySelector(".score").textContent = score;
-    } else {
-      document.querySelector(".message").textContent = "You lost the game!";
+      displayMessage("You lost the game!");
       document.querySelector(".score").textContent = 0;
     }
   }
 
   //Again button
   document.querySelector(".again").addEventListener("click", () => {
+    secretNumber = Math.trunc(Math.random() * 20) + 1;
     score = 20;
     document.querySelector(".score").textContent = score;
     document.querySelector(".number").textContent = "?";
     document.querySelector(".guess").value = "";
-    document.querySelector(".message").textContent = "Start guessing...";
+    displayMessage("Start guessing...");
     document.querySelector("body").style.backgroundColor = "#222";
     document.querySelector(".number").style.width = "15rem";
   });
